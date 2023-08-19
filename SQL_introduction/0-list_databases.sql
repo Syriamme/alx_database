0fadc6a4f6051e62d9a256a2abab4ddaa4483439
Syriamme <Roridaniel01@gmail.com>
2023-08-19 14:34:38 +0300

    a script that lists all databases of your MySQL server

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,12 +1,3 @@
 !/bin/bash
-
-/*This script lists all the databases in the connected MySQL server
-Execute the SQL command to list all databases
-*/
-
-mysql -u root -p -e 
-
-"
--- SQL command to show all databases
+-- Listing all the databases
 SHOW DATABASES;
-"
