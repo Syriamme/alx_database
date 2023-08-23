ea8cead35a3391155e869b1b896a6751a1b1fe03
Syriamme <Roridaniel01@gmail.com>
2023-08-23 20:38:20 +0300

    creates the database hbtn_0d_2 and the user user_0d_2

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,15 @@
+-- Create database if it doesn't exist
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create user if it doesn't exist
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege on hbtn_0d_2 database to user_0d_2
+
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Flush privileges to apply changes
+
+FLUSH PRIVILEGES;
