a11622425c6eca62a3d3e5e04be9f91ea7cb0cc6
Syriamme <Roridaniel01@gmail.com>
2023-08-23 20:06:32 +0300

    litsing all privileges of the MySQL users

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
---litsing all privileges of the MySQL users-- Lists all privileges of the MySQL users
-
+--litsing all privileges of the MySQL users
 
 -- Query retrieving privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
