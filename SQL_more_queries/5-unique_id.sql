6e3e45a1af6079b12dff17ecd917a94f60189eed
Syriamme <Roridaniel01@gmail.com>
2023-08-23 21:00:32 +0300

    a script that creates the table unique_id

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -2,8 +2,9 @@
 
 
 CREATE TABLE
-        IF NOT EXISTS unique_id (
+        IF NOT EXISTS unique_id 
+        (
             id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
             name VARCHAR(256),
             UNIQUE (id)
-            );
+        );
