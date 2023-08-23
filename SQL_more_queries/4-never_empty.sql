b00b5e722e3af668262a5f72b4fc5bb578251267
Syriamme <Roridaniel01@gmail.com>
2023-08-23 21:03:01 +0300

    a script that creates the table id_not_null

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -2,8 +2,9 @@
 
 
 CREATE TABLE
-        IF NOT EXISTS id_not_null (
-    id INT NOT NULL DEFAULT 1,
-
-    name VARCHAR(256)
-);
+        IF NOT EXISTS id_not_null
+        (
+            id INT NOT NULL DEFAULT 1,
+            
+            name VARCHAR(256)
+        );
