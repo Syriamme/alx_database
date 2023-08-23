55bfe509b2d1d4370a75d73cb3c3bbfb64f36918
Syriamme <Roridaniel01@gmail.com>
2023-08-23 20:08:15 +0300

    litsing all privileges of the MySQL users

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,4 @@
 --litsing all privileges of the MySQL users
 
--- Query retrieving privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- Query retrieving privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
